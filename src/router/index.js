acdf73f8aeefb0c04e18d3634cc20679cc070e1b
Constantine <constantine-x@outlook.com>
2020-09-15 01:14:42 +0300

    set router and main pages

--- src/router/index.js
+++ src/router/index.js
@@ -13,11 +13,26 @@ const routes = [
   {
     path: "/about",
     name: "About",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
     component: () =>
       import(/* webpackChunkName: "about" */ "../views/About.vue")
+  },
+  {
+    path: "/skills",
+    name: "Skills",
+    component: () =>
+      import(/* webpackChunkName: "about" */ "../views/Skills.vue")
+  },
+  {
+    path: "/projects",
+    name: "Projects",
+    component: () =>
+      import(/* webpackChunkName: "about" */ "../views/Projects.vue")
+  },
+  {
+    path: "/contacts",
+    name: "Contacts",
+    component: () =>
+      import(/* webpackChunkName: "about" */ "../views/Contacts.vue")
   }
 ];
 
