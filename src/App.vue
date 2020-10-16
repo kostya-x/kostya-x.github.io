d7f884de518cc0de2a9148f59e701841e098f8c3
Constantine <constantine-x@outlook.com>
2020-10-17 00:50:31 +0300

    remove navigation routes

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,6 @@
 <template>
   <div id="app">
     <the-header />
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link> |
-      <router-link to="/skills">Skills</router-link> |
-      <router-link to="/projects">Projects</router-link> |
-      <router-link to="/contacts">Contacts</router-link>
-    </div>
     <router-view />
   </div>
 </template>
