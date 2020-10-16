23c650d2cbf26d512cab0f5ca369a386864b5933
Constantine <constantine-x@outlook.com>
2020-10-16 23:00:17 +0300

    add header

---
+++ src/components/TheHeader.vue
@@ -0,0 +1,27 @@
+<template>
+  <header class="header">
+    <div class="logo"></div>
+  </header>
+</template>
+<style lang="scss">
+@import "../scss/_variables.scss";
+
+.header {
+  position: fixed;
+  width: 100%;
+  display: flex;
+  justify-content: space-between;
+  height: 64px;
+  background-color: $color-dark;
+  z-index: 1;
+}
+
+@media screen and (min-width: 1024px) {
+  .header {
+    width: 64px;
+    height: 100%;
+    display: flex;
+    flex-direction: column;
+  }
+}
+</style>
