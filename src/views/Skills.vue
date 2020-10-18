ba77242abb365faced03219d4a3dc72548393ffb
Constantine <constantine-x@outlook.com>
2020-10-18 04:00:20 +0300

    add skills content

--- src/views/Skills.vue
+++ src/views/Skills.vue
@@ -1,5 +1,39 @@
 <template>
   <div class="skills">
-    <h1>This is an skills page</h1>
+    <div class="main__conteiner">
+      <h1 class="main__title">
+        <span class="text--accent">Skills</span>
+      </h1>
+      <ul class="main__list">
+        <li>HTML, CSS JS</li>
+        <li>Emmet, Sass/SCSS, JSon</li>
+        <li>Git(GitHub), NPM, Gulp, ESLint, Babel</li>
+        <li>BEM, semantic, adaptive, validation</li>
+        <li>CSS animation, DOM manipulation, work with API</li>
+        <li>Graphics editors: Figma, Adobe Photoshop, CorelDraw</li>
+        <li>English: pre-intermedieate</li>
+      </ul>
+      <p class="main__text">
+        For more details you can download
+        <a
+          class="main__link"
+          href="https://github.com/kostya-x/kostya-x.github.io/raw/master/CV_front-end_developer_mikhailenko_konstantin.pdf"
+          download
+        >
+          my CV</a
+        >, visit my
+        <a
+          class="main__link"
+          href="https://www.linkedin.com/in/konstantin-mikhaylenko/"
+          target="_blank"
+          >LinkedIn</a
+        >
+        profile or just
+        <router-link class="main__link" to="/contacts">
+          contact
+        </router-link>
+        me.
+      </p>
+    </div>
   </div>
 </template>
