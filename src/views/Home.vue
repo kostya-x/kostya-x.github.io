08da1920938c234a4f518d7469cefbae9d29ecde
Constantine <constantine-x@outlook.com>
2020-10-18 03:48:17 +0300

    add home content

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,5 +1,18 @@
 <template>
-  <div class="home"></div>
+  <div class="home">
+    <div class="main__conteiner">
+      <h1 class="main__title">
+        Hi,<br />
+        I’m <span class="text--accent">K</span>ostya,<br />
+        Front-End Developer
+      </h1>
+      <div class="main__content">
+        <router-link class="main__contact" to="/contacts">
+          CONTACT ME
+        </router-link>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
